b55e57f78c4797dfee0de4a9f711738b6fc87375
obVdo <u.anandapadmanabhan@gmail.com>
2026-03-12 12:28:52 +0100

    add authors

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ app-inverse-v2: Create inverse operator for MEG/EEG source reconstruction.
 
 Authors : Guiomar Niso (guiomar.niso@gmail.com)
           Antonio Caulín (antoniocaulinatienzar@gmail.com) https://github.com/AntonioCauAt
+          Maximilien Chaumon https://github.com/dnacombo
+          obVdo https://github.com/obVdo
 Based on: https://github.com/guiomar/app-inverse-operator
 
 Inputs : fwd.fif (from app-forward-v2),
